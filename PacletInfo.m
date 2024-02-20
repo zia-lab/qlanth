922dc2a6ffe8a08e4564001f80cd0242c3d1fec2
Juan David Lizarazo Ferro <juan_lizarazoferro@brown.edu>
2024-02-19 20:15:15 -0500

    Tracking paclet.info

---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+Paclet[
+    Name -> "qlanth",
+    Version -> "1.0.0",
+    MathematicaVersion -> "13.2+",
+    Description -> "A Hamiltonian for the lanthanides.",
+    Creator -> "Juan David Lizarazo Ferro, Christopher Dodson",
+    Extensions -> 
+        {
+            {"Kernel", Root ->  ".", Context -> "qlanth`"}
+        }
+]
